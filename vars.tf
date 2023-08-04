8e4bb8f3a284e603998c2b424eb05e7fc0f90502
Sreeni002 <sreenu.reddy002@gmail.com>
2023-08-04 23:12:55 +0530

    setup docdb resource

---
+++ vars.tf
@@ -0,0 +1,13 @@
+variable "env" {}
+variable "subnets" {}
+variable "tags" {}
+variable "vpc_id" {}
+variable "allow_db_cidr" {}
+variable "engine_version" {}
+variable "kms_arn" {}
+variable "port_no" {
+  default = 27017
+}
+variable "name" {
+  default = "docdb"
+}
